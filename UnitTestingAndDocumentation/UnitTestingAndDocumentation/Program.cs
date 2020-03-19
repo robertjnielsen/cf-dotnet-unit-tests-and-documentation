4a64a1c9675f9a47d5bba28b1c94472e7de9f9f9
Robert James Nielsen <robert.j.nielsen@outlook.com>
2020-03-18 17:59:25 -0700

    Created UnitTestingAndDocumentation project / solution files.

---
+++ UnitTestingAndDocumentation/UnitTestingAndDocumentation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UnitTestingAndDocumentation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
