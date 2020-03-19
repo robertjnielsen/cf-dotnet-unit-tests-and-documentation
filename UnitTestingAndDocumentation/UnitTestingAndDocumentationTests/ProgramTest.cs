87bb6ee5472dc603de31bb234068faac2ebb1b8d
Robert James Nielsen <robert.j.nielsen@outlook.com>
2020-03-18 20:51:50 -0700

    Added two tests for DepositFunds method, both passed.

--- UnitTestingAndDocumentation/UnitTestingAndDocumentationTests/ProgramTest.cs
+++ UnitTestingAndDocumentation/UnitTestingAndDocumentationTests/ProgramTest.cs
@@ -12,7 +12,7 @@ namespace UnitTestingAndDocumentationTests
         [InlineData(30)]
         public void WithdrawFundsShouldReturnPositiveBalance(decimal testAmount)
         {
-            bool positiveBalance = Program.WithdrawFunds(testAmount) > 0;
+            bool positiveBalance = Program.WithdrawFunds(testAmount) <= Program.Balance;
             Assert.True(positiveBalance);
         }
 
@@ -20,8 +20,26 @@ namespace UnitTestingAndDocumentationTests
         public void WithdrawFundsShouldNotReturnNegativeBalance()
         {
             decimal testAmount = 100000;
-            bool negativeBalance = Program.WithdrawFunds(testAmount) > 0;
+            bool negativeBalance = Program.WithdrawFunds(testAmount) == Program.Balance;
             Assert.True(negativeBalance);
+        }
+
+        [Theory]
+        [InlineData(10)]
+        [InlineData(100)]
+        [InlineData(839.94)]
+        public void DepositFundsShouldOnlyDepositPositiveAmounts(decimal depositAmount)
+        {
+            bool positiveDeposit = Program.DepositFunds(depositAmount) > Program.Balance;
+            Assert.True(positiveDeposit);
+        }
+
+        [Fact]
+        public void DepositFundsShouldNotDepositNegativeAmounts()
+        {
+            decimal testAmount = -143.50M;
+            bool negativeDeposit = Program.DepositFunds(testAmount) == Program.Balance;
+            Assert.True(negativeDeposit);
         }
     }
 }
